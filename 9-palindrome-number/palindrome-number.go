eb5f003246764a213c30ec1f44d47cb1bb236e00
Andrian Aji <66558907+Andrianns@users.noreply.github.com>
2025-07-02 21:10:23 +0700

    Time: 8 ms (18.84%) | Memory: 6.4 MB (24.82%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.go
@@ -0,0 +1,19 @@
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+
+    str := strconv.Itoa(x)
+    left := 0
+    right := len(str) - 1
+
+    for left < right {
+        if str[left] != str[right]{
+            return false
+        }
+        left++
+        right--
+    }
+
+    return true
+}
